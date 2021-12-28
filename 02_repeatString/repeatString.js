082ca5c302c4e28a3bf1c75e59124be71c2c5d3e
Jesse Pantoja <pantojabruin@gmail.com>
2021-12-28 01:55:34 -0800

    Add missing semicolons to 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,10 @@
 const repeatString = function(string, num) {
 
-    if (num < 0) return 'ERROR'
+    if (num < 0) return 'ERROR';
 
-    const arr = []
-    for(let i = 0; i < num; i++) arr.push(string)
-    return arr.join("")
+    const arr = [];
+    for(let i = 0; i < num; i++) arr.push(string);
+    return arr.join("");
 };
 
 // Do not edit below this line
