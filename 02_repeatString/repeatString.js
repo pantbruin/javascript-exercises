847b624c7aa46dead722245c1236da7ac1f1ac4f
Jesse Pantoja <pantojabruin@gmail.com>
2021-12-28 01:55:06 -0800

    Add solution to exercise 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) return 'ERROR'
+
+    const arr = []
+    for(let i = 0; i < num; i++) arr.push(string)
+    return arr.join("")
 };
 
 // Do not edit below this line
