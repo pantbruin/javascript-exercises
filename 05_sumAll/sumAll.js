fcbc17c5b1ebfbb9975f47799f1fec98a58c8d27
Jesse Pantoja <pantojabruin@gmail.com>
2021-12-28 21:26:29 -0800

    Add missing semicolons

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,12 @@
 const sumAll = function(num1, num2) {
 
-    if (num1 < 0 || num2 < 0 || !(typeof num1 === 'number') || !(typeof num2 === 'number')) return 'ERROR'
+    if (num1 < 0 || num2 < 0 || !(typeof num1 === 'number') || !(typeof num2 === 'number')) return 'ERROR';
 
     let sum = 0;
-    for (let i = num1 < num2 ? num1 : num2; i <= (num2 > num1 ? num2 : num1); i++) sum += i
-    return sum
+
+    for (let i = num1 < num2 ? num1 : num2; i <= (num2 > num1 ? num2 : num1); i++) sum += i;
+
+    return sum;
 };
 
 // Do not edit below this line
