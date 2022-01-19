32a7b1b35970b9ad77f9159d319d9ea604d4f835
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-18 19:41:45 -0800

    Solve javascript exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    // iterative 
+    num = Number(num)
+    if (num <= 0 ) return "OOPS"
+    const cache = [0, 1]
+    
+    for (let i = 2; i <= num; i++){
+        cache[i] = cache[i - 1] + cache[i - 2]
+    }
+    return cache[num]
 };
 
 // Do not edit below this line
