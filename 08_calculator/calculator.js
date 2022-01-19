955c292d5d30ce97a0eb9983350ccac87b37a5f8
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-18 18:59:34 -0800

    Complete js exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b
+};
+
+const subtract = function(a, b) {
+	return a - b
+};
+
+const sum = function(arr) {
+	return (arr.reduce((sum, element) => {
+    return sum + element
+  }, 0))
+};
+
+const multiply = function(arr) {
+
+  return (arr.reduce((accum, element) => {
+    return accum * element
+  }, 1))
+
+};
+
+const power = function(a, b) {
+
+  return a ** b
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  //recursive
+  if (num === 0) return 1
+  return num * factorial(num - 1)
 };
 
 // Do not edit below this line
