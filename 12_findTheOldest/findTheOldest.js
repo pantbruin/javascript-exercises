19c6db767cb1625038dcee362b8a77c2ced570a5
Jesse Pantoja <pantojabruin@gmail.com>
2022-01-19 19:19:30 -0800

    Solve javascript exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const isObjectEmpty = (obj) => {
+    for (const key in obj){
+        return false;
+    };
+    return true;
+};
 
+
+const findTheOldest = function(peopleArray) {
+    let result = peopleArray.reduce((oldestPersonObj, currentPersonObj) => {
+
+        if (isObjectEmpty(oldestPersonObj)) return currentPersonObj
+
+        let currYear = new Date().getFullYear();
+
+        let currPersonAge = 'yearOfDeath' in currentPersonObj ? currentPersonObj.yearOfDeath - currentPersonObj.yearOfBirth : currYear - currentPersonObj.yearOfBirth;
+        let oldestPersonAge = 'yearOfDeath' in oldestPersonObj ? oldestPersonObj.yearOfDeath - oldestPersonObj.yearOfBirth : currYear - oldestPersonObj.yearOfBirth;
+
+        oldestPersonObj = currPersonAge > oldestPersonAge ? currentPersonObj : oldestPersonObj;
+        return oldestPersonObj
+        
+    }, {});
+    return result;
 };
 
 // Do not edit below this line
